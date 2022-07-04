86b41b547ab10a4930f858290129d926fa31a449
ABWEALTHY-LX <ebenezeryaiperepolu@gmail.com>
2022-07-04 04:10:40 -0700

    Positive or Negative

--- 0x01-variable_if_else_while/0-positive_or_negative.c
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - assigns a random number to int n everytime
- * it executes, and prints it
+ * main - assigns a random number to int n everytime it executes, and prints it
+ *
  * Return: Always 0 (Success)
  */
 
@@ -33,3 +33,5 @@ int main(void)
 	return (0);
 
 }
+
+
