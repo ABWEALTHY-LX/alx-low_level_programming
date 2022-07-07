b816a8a88fde2e40997f01c7f109a1ad30116591
ABWEALTHY-LX <ebenezeryaiperepolu@gmail.com>
2022-07-06 22:02:08 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,8 +8,17 @@
  */
 int main(void)
 {
-	putchar("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
 	return (0);
 
-}
+
 
