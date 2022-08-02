e52a33911b9ac43d6e9fc089f6e169be585dcfef
ABWEALTHY-LX <ebenezeryaiperepolu@gmail.com>
2022-08-02 03:04:44 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,7 +8,7 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d)
+		if (d)
 		{
 		if (!(d->name))
 			printf("Name: (nil)\n");
@@ -18,5 +18,6 @@ void print_dog(struct dog *d)
 		if (!(d->owner))
 			printf("Owner: (nil)\n");
 		else
-			printf("Owner: %s\n", d->owner);			}
+			printf("Owner: %s\n", d->owner);
+		}
 }
