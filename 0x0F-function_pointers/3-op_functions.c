9c49610b03bfcf648457ba14fbca7848fc14cda6
ABWEALTHY-LX <ebenezeryaiperepolu@gmail.com>
2022-08-03 11:10:00 -0700

    function

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -43,8 +43,8 @@ int op_div(int a, int b)
 {
 		if (b == 0)
 		{
-		printf("Error\n");
-		exit(100);
+			printf("Error\n");
+			exit(100);
 		}
 		return (a / b);
 }
@@ -59,8 +59,8 @@ int op_mod(int a, int b)
 {
 		if (b == 0)
 		{
-		printf("Error\n");
-		exit(100);
+			printf("Error\n");
+			exit(100);
 		}
-			return (a % b);
+		return (a % b);
 }
