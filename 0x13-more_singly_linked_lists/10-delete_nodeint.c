7489ac37266a84509aabeffe18c7f568186ad6ca
ABWEALTHY-LX <ebenezeryaiperepolu@gmail.com>
2022-08-16 19:24:57 -0700

    delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			return (-1);
 		}
+
 		next = prev->next;
+
 		if (index != 0)
 		{
 			prev->next = next->next;
@@ -38,6 +40,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			free(prev);
 			*head = next;
 		}
+
 		return (1);
 }
 
